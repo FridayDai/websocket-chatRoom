13cd8c4947e6038d47b3e649b3c1a6d33f7fd906
yi.dai <yi.dai@envisioncn.com>
2019-02-26 17:20:08 +0800

    develop websocket server

---
+++ src/index.js
@@ -0,0 +1,37 @@
+/**
+ * Created by yi.dai on 2019/2/26.
+ */
+// https://github.com/websockets/ws
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({
+  port: 65534,
+  perMessageDeflate: {
+    zlibDeflateOptions: {
+      // See zlib defaults.
+      chunkSize: 1024,
+      memLevel: 7,
+      level: 3
+    },
+    zlibInflateOptions: {
+      chunkSize: 10 * 1024
+    },
+    // Other options settable:
+    clientNoContextTakeover: true, // Defaults to negotiated value.
+    serverNoContextTakeover: true, // Defaults to negotiated value.
+    serverMaxWindowBits: 10, // Defaults to negotiated value.
+    // Below options specified as default values.
+    concurrencyLimit: 10, // Limits zlib concurrency for perf.
+    threshold: 1024 // Size (in bytes) below which messages
+    // should not be compressed.
+  }
+});
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    ws.send(message);
+  });
+});
+
+console.log('websocket server is running...');
